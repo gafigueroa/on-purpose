42f8bb24700fee264d2b6187bf27ffc7bc0908db
Guillermo Figueroa <gafigueroa@uc.cl>
2017-02-07 17:22:08 -0800

    Starting new task

---
+++ js/new-task.js
@@ -0,0 +1,17 @@
+'use strict';
+
+$(document).ready(function() {
+	initializePage();
+	$("#description-inside").hide();
+	$("#duration-inside").hide();
+})
+
+function initializePage() {
+
+
+
+}
+
+$(".information-input h3").click(function(){
+     alert('clicked')
+   });
